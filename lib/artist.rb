4730871fe87eec3b928e9a80b62fdb4ff7a9beb5
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-20 20:43:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ class Artist
   def self.find_or_create_by_name(name)
     if find_by_name(name)
       find_by_name(name)
-    else name = self.new
+    else name = self.new(name)
     end 
   end 
   
